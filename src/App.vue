a675d427860495d936b075bc7ca77e9f73d637a4
organics <374564811@qq.com>
2022-09-05 21:52:36 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import XtermDemoVue from './components/XtermDemo.vue'
+
 </script>
 
 <template>
@@ -14,6 +16,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <XtermDemoVue/>
 </template>
 
 <style scoped>
--- src/components/XtermDemo.vue
+++ src/components/XtermDemo.vue
@@ -1,36 +1,26 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+
+import { ref, onMounted } from 'vue'
 import { Terminal } from 'xterm'
-import xterm.css
+import 'xterm/css/xterm.css'
+
+
+const terminal = ref()
+
+onMounted(() => {
+  console.log(terminal.value);// <div>
+
+  const term = new Terminal();
+
+  term.open(terminal.value);
+  term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+})
+
 </script>
-
+  
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div ref="terminal"></div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
 </style>
